97b07b18268c82107585a603e5dc300b8ad97655
Oliver Sharpe <osharpe@spartaglobal.com>
2018-11-18 13:28:03 +0000

    V.1.8.0 added color by type

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,5 @@
 class Pokemon < ApplicationRecord
   belongs_to :generation
+  validates :name, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :generation_id, :numericality => { :greater_than_or_equal_to => 0 }
 end
