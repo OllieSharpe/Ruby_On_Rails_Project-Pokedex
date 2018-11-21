7369bce32bd3a476c18ccccd6880210634653b6d
Oliver Sharpe <osharpe@spartaglobal.com>
2018-11-21 13:26:28 +0000

    V.1.8.1 fixed validations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,5 +1,4 @@
 class Pokemon < ApplicationRecord
   belongs_to :generation
-  validates :name, :numericality => { :greater_than_or_equal_to => 0 }
   validates :generation_id, :numericality => { :greater_than_or_equal_to => 0 }
 end
